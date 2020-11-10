26b723ca022a6b74ba0122609995075c97a63e0f
mlgmontage <kettleofrassel1@gmail.com>
2020-11-10 14:36:48 +0500

    notes create route

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const noteSchema = require("../schemas/note");
 
 router.get("/", async (req, res) => {
   const notes = await prisma.notes.findMany();
   res.json(notes);
+});
+
+router.post("/create", async (req, res) => {
+  const body = req.body;
+
+  if (!noteSchema.validate(body).error) {
+    const created = await prisma.notes.create({
+      data: body,
+    });
+
+    res.json(created);
+  } else {
+    res.status(400).json(noteSchema.validate(body).error.details);
+  }
 });
 
 module.exports = router;
